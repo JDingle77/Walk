e80cec7422fe5bfa21fe7afdc75d2a9024559328
James Dingle <jdingle7@g.ucla.edu>
2021-11-29 18:18:31 -0800

    prettier and fixed weird formatting

--- frontend/screens/home.tsx
+++ frontend/screens/home.tsx
@@ -1,32 +1,30 @@
-import * as React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
-import { Text, View } from '../components/Themed';
+import * as React from "react";
+import { SafeAreaView, StyleSheet } from "react-native";
+import { Text, View } from "../components/Themed";
 
 const Home = () => {
-    return (
-        <SafeAreaView>
-            <View style={styles.container}>
-                <Text style={styles.title}>This is the Home Page</Text>
-            </View>
-        </SafeAreaView>
-    );
-}
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>This is the Home Page</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    title: {
-      fontSize: 20,
-      fontWeight: 'bold',
-    },
-    separator: {
-      marginVertical: 30,
-      height: 1,
-      width: '80%',
-    },
-  });
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+});
 
-  export default Home;
+export default Home;
--- frontend/screens/login.tsx
+++ frontend/screens/login.tsx
@@ -1,64 +1,69 @@
-import * as React from 'react';
-import { useState } from 'react';
-import { Alert, SafeAreaView, StyleSheet } from 'react-native';
-import { Button } from 'react-native-paper';
-import TextInput from '../components/TextInput';
-import { View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import * as React from "react";
+import { useState } from "react";
+import { Alert, SafeAreaView, StyleSheet } from "react-native";
+import { Button } from "react-native-paper";
+import TextInput from "../components/TextInput";
+import { View } from "../components/Themed";
+import { RootTabScreenProps } from "../types";
 
-const Login = ({ navigation }: RootTabScreenProps<'Login'>) => {
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState({ value: '', error: '' });
-  const [password, setPassword] = useState({ value: '', error: '' });
+const Login = ({ navigation }: RootTabScreenProps<"Login">) => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState({ value: "", error: "" });
+  const [password, setPassword] = useState({ value: "", error: "" });
 
   return (
-    <SafeAreaView>
-      <View style={styles.container}>
-        <TextInput
-            label="Your email"
-            enablesReturnKeyAutomatically
-            value={email.value}
-            onChangeText={text => setEmail({ value: text, error: '' })}
-            error={!!email.error}
-            errorText={email.error}
-            autoCapitalize="none"
-            autoComplete="email"
-            textContentType="emailAddress"
-        />
-        <TextInput
-            label="Your password"
-            enablesReturnKeyAutomatically
-            value={password.value}
-            onChangeText={text => setPassword({ value: text, error: '' })}
-            error={!!password.error}
-            errorText={password.error}
-            autoCapitalize="none"
-            autoComplete="none"
-            textContentType="password"
-        />
-        <Button color={'white'} onPress={() => email.value != '' && password.value != '' ? navigation.navigate('Home') : Alert.alert('Please fill out both fields.')}>
-          Sign In
-        </Button>
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <TextInput
+        label="Your email"
+        enablesReturnKeyAutomatically
+        value={email.value}
+        onChangeText={(text) => setEmail({ value: text, error: "" })}
+        error={!!email.error}
+        errorText={email.error}
+        autoCapitalize="none"
+        autoComplete="email"
+        textContentType="emailAddress"
+      />
+      <TextInput
+        label="Your password"
+        enablesReturnKeyAutomatically
+        value={password.value}
+        onChangeText={(text) => setPassword({ value: text, error: "" })}
+        error={!!password.error}
+        errorText={password.error}
+        autoCapitalize="none"
+        autoComplete="none"
+        textContentType="password"
+      />
+      <Button
+        color={"black"}
+        onPress={() =>
+          email.value != "" && password.value != ""
+            ? navigation.navigate("Home")
+            : Alert.alert("Please fill out both fields.")
+        }
+      >
+        Sign In
+      </Button>
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column',
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
 
