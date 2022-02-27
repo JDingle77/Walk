445f54dd666e4d1b451eb6d6b6de4972b152b940
futoidaddy <57603310+hametar0u@users.noreply.github.com>
2022-02-27 15:04:47 -0800

    added image and view components

--- frontend/screens/Summary.tsx
+++ frontend/screens/Summary.tsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import { StyleSheet, Text, Image } from 'react-native';
 import { View } from '../components/Themed';
+import { Button } from "react-native-paper";
 
 export default function SummaryPage() {
   return (
     <View style={styles.container} >
-      <Text style={styles.title} >
-        Summary Page
-      </Text>
-      <Image 
-        source={require('../assets/images/summary-image.png')} 
-      />
+      <View style={{ width: '100%' }}>
+        <Text style={styles.title} >
+          Summary Page
+        </Text>
+        <Image 
+          style={styles.image}
+          source={require('../assets/images/summary-image.png')} 
+          resizeMode="contain"
+        />
+      </View>
+      <View style={styles.container}> 
+        <Text style={styles.h1}>Nov 4 2021 - Casual Stroll</Text>
+      </View>
+      <Button color={"black"} > 
+        Continue
+      </Button>
     </View>
   );
 }
@@ -22,12 +33,22 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   title: {
-    fontSize: 20,
+    fontSize: 40,
     fontWeight: "bold",
+    textAlign: "left",
+    fontFamily: "braveold",
+    marginBottom: 30,
+  },
+  image: {
+    width: '100%',
   },
   separator: {
     marginVertical: 30,
     height: 1,
     width: "80%",
+  },
+  h1: {
+    fontSize: 30,
+    color: '#000000',
   },
 });
