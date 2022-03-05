3962f7c6bfed4b516266c07e5450ea30c07290c3
futoidaddy <57603310+hametar0u@users.noreply.github.com>
2022-03-04 17:48:12 -0800

    decently set up UI summary page

--- frontend/screens/Summary.tsx
+++ frontend/screens/Summary.tsx
@@ -2,78 +2,95 @@ import React from 'react'
 import { StyleSheet, Text, Image, SafeAreaView, FlatList } from 'react-native';
 import { View } from '../components/Themed';
 import { Button } from "react-native-paper";
+import { pink100 } from 'react-native-paper/lib/typescript/styles/colors';
 
 export default function SummaryPage() {
   const recipes = [ //test data; get data later
     {
         id: 0,
         title: 'Distance',
-        cook_time: '0.20 mi',
+        data: '0.20 mi',
     },
     {
         id: 1,
         title: 'Time',
-        cook_time: '00:04:32',
+        data: '00:04:32',
     },
     {
         id: 2,
         title: 'Avg Speed (mph)',
-        cook_time: '2.65',
+        data: '2.65',
     },
     {
         id: 3,
         title: 'Pee Stops',
-        cook_time: '5',
+        data: '5',
     },
     {
         id: 4,
         title: 'Poop Drops',
-        cook_time: '3',
+        data: '3',
     },
     {
         id: 5,
         title: 'Water Breaks',
-        cook_time: '2',
+        data: '2',
     }
   ];
 
   return (
     <SafeAreaView style={styles.container} >
-      <View style={{height: '10%', width: '90%', borderWidth: 3, borderColor: 'green'}}>
-        <Text style={styles.title} >
-          Summary
-        </Text>
+      <View style={styles.topContainer}>
+        <View style={styles.titleView}>
+          <Text style={styles.title} >
+            Summary
+          </Text>
+        </View>
+        <View style={{height: '65%'}}>
+          <Image 
+            style={styles.image}
+            source={require('../assets/images/summary-image.png')} 
+            resizeMode="contain"
+          />
+        </View>
+        <View style={styles.speedBarContainer}>
+          <Text style={styles.p}>Slow</Text>
+          <Image 
+            style={styles.image}
+            source={require('../assets/images/speed-bar.png')} 
+            resizeMode="contain"
+          />
+          <Text style={styles.p}>Fast</Text>
+        </View>
       </View>
-      <View style={{height: 300}}>
-        <Image 
-          style={styles.image}
-          source={require('../assets/images/summary-image.png')} 
-          resizeMode="contain"
-        />
-      </View>
-      <View style={styles.header}>
-        <Text style={styles.h1}>Nov 4 2021 - Casual Stroll</Text>
-      </View>
+      <View style={styles.bottomContainer}>
+        <View style={styles.header}>
+          <Text style={styles.h1}>Nov 4 2021 - Casual Stroll</Text>
+        </View>
 
-      <FlatList
-        style={{width:'100%', borderWidth: 3, borderColor: 'red'}}
-        data={recipes}
-        renderItem={(obj) => {
-            return (
-                <View style={{flex: 1, flexDirection: 'row', borderWidth: 1, borderColor: 'orange'}}>
-                  <View style={styles.cellLeft}>
-                    <Text style={styles.p}>{obj.item.title}</Text>
+        <FlatList
+          style={styles.list}
+          data={recipes}
+          renderItem={(obj) => {
+              return (
+                  <View style={styles.row}>
+                    <View style={styles.cell}>
+                      <Text style={styles.p}>{obj.item.title}</Text>
+                    </View>
+                    <View style={styles.cell}>
+                      <Text style={[styles.p,  {textAlign: 'right'}]}>{obj.item.data}</Text>
+                    </View>
                   </View>
-                  <View style={styles.cellRight}>
-                    <Text style={[styles.p,  {textAlign: 'right'}]}>{obj.item.cook_time}</Text>
-                  </View>
-                </View>
-            );
-        }}
-        />
+              );
+          }}
+          />
+
       <Button style={styles.button} labelStyle={styles.buttonLabel}> 
         Continue
       </Button>
+      </View>
+      
+      
     </SafeAreaView>
   );
 }
@@ -84,9 +101,31 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: 'rgb(243,239,226)',
-    borderWidth: 3,
-    borderColor: 'blue',
+    backgroundColor: 'rgb(252,251,247)',
+    // borderWidth: 3,
+    // borderColor: 'blue',
+  },
+  topContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "rgb(244,238,225)",
+    // borderWidth: 3,
+    // borderColor: 'blue',
+  },
+  bottomContainer: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: 'rgb(252,251,247)',
+    // borderWidth: 3,
+    // borderColor: "pink",
+  },
+  titleView: {
+    flex: 1,
+    width: '90%', 
+    // borderWidth: 3, 
+    // borderColor: 'green'
   },
   title: {
     flex: 1,
@@ -97,36 +136,50 @@ const styles = StyleSheet.create({
   },
   image: {
     flex: 1,
+    // borderWidth: 1,
     // width: '100%',
-    // height: '30%',
-    marginBottom: 20,
+    height: '30%',
+    // marginBottom: 20,
   },
   separator: {
     marginVertical: 30,
     height: 1,
     width: "80%",
   },
+  speedBarContainer: {
+    flex: 1, 
+    flexDirection: 'row', 
+    width: '80%', 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    // borderWidth: 1
+  },
   header: {
     height: 30,
     backgroundColor: 'rgb(252,251,247)',
     width: '100%',
-    flex: 0.2,
+    flex: 0.3,
   },
-  table: {
-    backgroundColor: 'rgb(252,251,247)',
+  list : {
+    flex: 1,
+    width:'100%', 
+    // borderWidth: 3, 
+    // borderColor: 'red'
+  },
+  row: {
     flex: 1, 
-    flexDirection:'column', 
-    width: '100%',
-  },
-  cellLeft: {
-    width: '50%', 
-    borderWidth: 1, 
-    // borderColor: 'purple', //testing
-  },
-  cellRight: {
-    width: '50%', 
+    flexDirection: 'row', 
+    alignSelf: 'center',
+    width: '95%',
     // borderWidth: 1, 
-    // borderColor: 'pink', //testing
+    // borderColor: 'orange',
+  },
+  cell: {
+    width: '50%', 
+    padding: 5,
+    // borderWidth: 1, 
+    backgroundColor: 'rgb(252,251,247)',
+    // borderColor: 'purple', //testing
   },
   h1: {
     fontSize: 24,
@@ -143,6 +196,7 @@ const styles = StyleSheet.create({
     letterSpacing: 0.1,
   },
   button: {
+    alignSelf: 'center',
     backgroundColor: 'rgb(226,186,108)',
     color: 'black',
     borderRadius: 28,
