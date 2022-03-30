975ee89e3a329c47f4ad660523983eeccde40584
futoidaddy <57603310+hametar0u@users.noreply.github.com>
2022-03-29 19:57:26 -0700

    Update Summary.tsx

--- frontend/screens/Summary.tsx
+++ frontend/screens/Summary.tsx
@@ -4,218 +4,218 @@ import { View } from '../components/Themed';
 import { Button } from "react-native-paper";
 import styles from '../stylesheets/globalStyles'
 
-export default class SummaryPage extends React.Component<any, any> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      "data": [ //test data; get data later
-        // {
-        //     id: 0,
-        //     title: 'Distance',
-        //     data: '0.20 mi',
-        // },
-        // {
-        //     id: 1,
-        //     title: 'Time',
-        //     data: '00:04:32',
-        // },
-        // {
-        //     id: 2,
-        //     title: 'Avg Speed (mph)',
-        //     data: '2.65',
-        // },
-        // {
-        //     id: 3,
-        //     title: 'Pee Stops',
-        //     data: '5',
-        // },
-        // {
-        //     id: 4,
-        //     title: 'Poop Drops',
-        //     data: '3',
-        // },
-        // {
-        //     id: 5,
-        //     title: 'Water Breaks',
-        //     data: '2',
-        // }
-      ],
-    }
-    
-  }
-  
-  async componentDidMount() {
-    try {
-      const res = await fetch(
-        'http://localhost:8000/maps/get_summary', {
-          method: "GET",
-          credentials: 'same-origin',
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application.json",
-          }
-        }
-      );
-      const data = await res.json();
-      console.log(data)
-      this.setState({
-        data: data
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  render () {
-    return (
-      <SafeAreaView style={styles.container} >
-        <View style={localStyles.topContainer}>
-          <View style={styles.titleView}>
-            <Text style={styles.title} >
-              Summary
-            </Text>
-          </View>
-          <View style={{height: '65%'}}>
-            <Image 
-              style={styles.image}
-              source={require('../assets/images/summary-image.png')} 
-              resizeMode="contain"
-            />
-          </View>
-          <View style={localStyles.speedBarContainer}>
-            <Text style={styles.p}>Slow</Text>
-            <Image 
-              style={styles.image}
-              source={require('../assets/images/speed-bar.png')} 
-              resizeMode="contain"
-            />
-            <Text style={styles.p}>Fast</Text>
-          </View>
-        </View>
-        <View style={localStyles.bottomContainer}>
-          <View style={styles.header}>
-            <Text style={styles.h1}>Nov 4 2021 - Casual Stroll</Text>
-          </View>
-  
-          <FlatList
-            style={styles.list}
-            data={this.state.recipes}
-            renderItem={(obj) => {
-                return (
-                    <View style={styles.row}>
-                      <View style={styles.cell}>
-                        <Text style={styles.p}>{obj.item.title}</Text>
-                      </View>
-                      <View style={styles.cell}>
-                        <Text style={[styles.p,  {textAlign: 'right'}]}>{obj.item.data}</Text>
-                      </View>
-                    </View>
-                );
-            }}
-            />
-  
-        <Button style={styles.button} labelStyle={styles.buttonLabel}> 
-          Continue
-        </Button>
-        </View>
-        
-        
-      </SafeAreaView>
-    );
-  }
-  
-}
-
-// export default function SummaryPage() {
-//   const recipes = [ //test data; get data later
-//     {
-//         id: 0,
-//         title: 'Distance',
-//         data: '0.20 mi',
-//     },
-//     {
-//         id: 1,
-//         title: 'Time',
-//         data: '00:04:32',
-//     },
-//     {
-//         id: 2,
-//         title: 'Avg Speed (mph)',
-//         data: '2.65',
-//     },
-//     {
-//         id: 3,
-//         title: 'Pee Stops',
-//         data: '5',
-//     },
-//     {
-//         id: 4,
-//         title: 'Poop Drops',
-//         data: '3',
-//     },
-//     {
-//         id: 5,
-//         title: 'Water Breaks',
-//         data: '2',
+// export default class SummaryPage extends React.Component<any, any> {
+//   constructor(props: any) {
+//     super(props);
+//     this.state = {
+//       "data": [ //test data; get data later
+//         // {
+//         //     id: 0,
+//         //     title: 'Distance',
+//         //     data: '0.20 mi',
+//         // },
+//         // {
+//         //     id: 1,
+//         //     title: 'Time',
+//         //     data: '00:04:32',
+//         // },
+//         // {
+//         //     id: 2,
+//         //     title: 'Avg Speed (mph)',
+//         //     data: '2.65',
+//         // },
+//         // {
+//         //     id: 3,
+//         //     title: 'Pee Stops',
+//         //     data: '5',
+//         // },
+//         // {
+//         //     id: 4,
+//         //     title: 'Poop Drops',
+//         //     data: '3',
+//         // },
+//         // {
+//         //     id: 5,
+//         //     title: 'Water Breaks',
+//         //     data: '2',
+//         // }
+//       ],
 //     }
-//   ];
+    
+//   }
+  
+//   async componentDidMount() {
+//     try {
+//       const res = await fetch(
+//         'http://localhost:8000/maps/get_summary', {
+//           method: "GET",
+//           credentials: 'same-origin',
+//           headers: {
+//             Accept: "application/json",
+//             "Content-Type": "application.json",
+//           }
+//         }
+//       );
+//       const data = await res.json();
+//       console.log(data)
+//       this.setState({
+//         data: data
+//       });
+//     } catch (e) {
+//       console.log(e);
+//     }
+//   }
 
-//   return (
-//     <SafeAreaView style={styles.container} >
-//       <View style={localStyles.topContainer}>
-//         <View style={styles.titleView}>
-//           <Text style={styles.title} >
-//             Summary
-//           </Text>
+//   render () {
+//     return (
+//       <SafeAreaView style={styles.container} >
+//         <View style={localStyles.topContainer}>
+//           <View style={styles.titleView}>
+//             <Text style={styles.title} >
+//               Summary
+//             </Text>
+//           </View>
+//           <View style={{height: '65%'}}>
+//             <Image 
+//               style={styles.image}
+//               source={require('../assets/images/summary-image.png')} 
+//               resizeMode="contain"
+//             />
+//           </View>
+//           <View style={localStyles.speedBarContainer}>
+//             <Text style={styles.p}>Slow</Text>
+//             <Image 
+//               style={styles.image}
+//               source={require('../assets/images/speed-bar.png')} 
+//               resizeMode="contain"
+//             />
+//             <Text style={styles.p}>Fast</Text>
+//           </View>
 //         </View>
-//         <View style={{height: '65%'}}>
-//           <Image 
-//             style={styles.image}
-//             source={require('../assets/images/summary-image.png')} 
-//             resizeMode="contain"
-//           />
-//         </View>
-//         <View style={localStyles.speedBarContainer}>
-//           <Text style={styles.p}>Slow</Text>
-//           <Image 
-//             style={styles.image}
-//             source={require('../assets/images/speed-bar.png')} 
-//             resizeMode="contain"
-//           />
-//           <Text style={styles.p}>Fast</Text>
-//         </View>
-//       </View>
-//       <View style={localStyles.bottomContainer}>
-//         <View style={styles.header}>
-//           <Text style={styles.h1}>Nov 4 2021 - Casual Stroll</Text>
-//         </View>
-
-//         <FlatList
-//           style={styles.list}
-//           data={recipes}
-//           renderItem={(obj) => {
-//               return (
-//                   <View style={styles.row}>
-//                     <View style={styles.cell}>
-//                       <Text style={styles.p}>{obj.item.title}</Text>
+//         <View style={localStyles.bottomContainer}>
+//           <View style={styles.header}>
+//             <Text style={styles.h1}>Nov 4 2021 - Casual Stroll</Text>
+//           </View>
+  
+//           <FlatList
+//             style={styles.list}
+//             data={this.state.recipes}
+//             renderItem={(obj) => {
+//                 return (
+//                     <View style={styles.row}>
+//                       <View style={styles.cell}>
+//                         <Text style={styles.p}>{obj.item.title}</Text>
+//                       </View>
+//                       <View style={styles.cell}>
+//                         <Text style={[styles.p,  {textAlign: 'right'}]}>{obj.item.data}</Text>
+//                       </View>
 //                     </View>
-//                     <View style={styles.cell}>
-//                       <Text style={[styles.p,  {textAlign: 'right'}]}>{obj.item.data}</Text>
-//                     </View>
-//                   </View>
-//               );
-//           }}
-//           />
-
-//       <Button style={styles.button} labelStyle={styles.buttonLabel}> 
-//         Continue
-//       </Button>
-//       </View>
-      
-      
-//     </SafeAreaView>
-//   );
+//                 );
+//             }}
+//             />
+  
+//         <Button style={styles.button} labelStyle={styles.buttonLabel}> 
+//           Continue
+//         </Button>
+//         </View>
+        
+        
+//       </SafeAreaView>
+//     );
+//   }
+  
 // }
+
+export default function SummaryPage() {
+  const recipes = [ //test data; get data later
+    {
+        id: 0,
+        title: 'Distance',
+        data: '0.20 mi',
+    },
+    {
+        id: 1,
+        title: 'Time',
+        data: '00:04:32',
+    },
+    {
+        id: 2,
+        title: 'Avg Speed (mph)',
+        data: '2.65',
+    },
+    {
+        id: 3,
+        title: 'Pee Stops',
+        data: '5',
+    },
+    {
+        id: 4,
+        title: 'Poop Drops',
+        data: '3',
+    },
+    {
+        id: 5,
+        title: 'Water Breaks',
+        data: '2',
+    }
+  ];
+
+  return (
+    <SafeAreaView style={styles.container} >
+      <View style={localStyles.topContainer}>
+        <View style={styles.titleView}>
+          <Text style={styles.title} >
+            Summary
+          </Text>
+        </View>
+        <View style={{height: '65%'}}>
+          <Image 
+            style={styles.image}
+            source={require('../assets/images/summary-image.png')} 
+            resizeMode="contain"
+          />
+        </View>
+        <View style={localStyles.speedBarContainer}>
+          <Text style={styles.p}>Slow</Text>
+          <Image 
+            style={styles.image}
+            source={require('../assets/images/speed-bar.png')} 
+            resizeMode="contain"
+          />
+          <Text style={styles.p}>Fast</Text>
+        </View>
+      </View>
+      <View style={localStyles.bottomContainer}>
+        <View style={styles.header}>
+          <Text style={styles.h1}>Nov 4 2021 - Casual Stroll</Text>
+        </View>
+
+        <FlatList
+          style={styles.list}
+          data={recipes}
+          renderItem={(obj) => {
+              return (
+                  <View style={styles.row}>
+                    <View style={styles.cell}>
+                      <Text style={styles.p}>{obj.item.title}</Text>
+                    </View>
+                    <View style={styles.cell}>
+                      <Text style={[styles.p,  {textAlign: 'right'}]}>{obj.item.data}</Text>
+                    </View>
+                  </View>
+              );
+          }}
+          />
+
+      <Button style={styles.button} labelStyle={styles.buttonLabel}> 
+        Continue
+      </Button>
+      </View>
+      
+      
+    </SafeAreaView>
+  );
+}
 
 const localStyles = StyleSheet.create({
   topContainer: {
