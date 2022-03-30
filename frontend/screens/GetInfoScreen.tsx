9bfaf341ce8b736fc281773951da26735acf4287
Jack Lin <jacklin1229@gmail.com>
2022-03-30 10:47:29 -0700

    fixed keyboardavoidview issue on getinfo page

--- frontend/screens/GetInfoScreen.tsx
+++ frontend/screens/GetInfoScreen.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { SafeAreaView, StyleSheet, Dimensions, KeyboardAvoidingView, Platform} from "react-native";
+import { Text, View, SafeAreaView, StyleSheet, Dimensions, KeyboardAvoidingView, Platform} from "react-native";
 import TextInput from "../components/TextInput";
-import { Text, View } from "../components/Themed";
 import { Button } from "react-native-paper";
 import styles from '../stylesheets/globalStyles'
 import { ScrollView } from "react-native-gesture-handler";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { Input } from "react-native-elements";
 
 // import {
 //   useFonts,
@@ -30,193 +31,237 @@ const GetInfoScreen = () => {
     const [gender, setGender] = useState("");
     const [birthday, setBirthday] = useState("");
     const [location, setLocation] = useState("");
+    const [shift, setShift] = useState(false)
     
     function uploadInfo() {
         console.log("Success")
     }
 
   return (
+    <KeyboardAvoidingView 
+      style = {stylesheet.backGround}
+      behavior={"position"}
+      enabled = {shift}
+      //enabled
+     // keyboardVerticalOffset={Platform.select({ios: 500, android: 500})}
+      >
+          <View style={stylesheet.textView}>
+            <Text style={stylesheet.titleText}>Let's get some Info!</Text>
+          </View>
+          <View style={stylesheet._Text_Field_Breed}>
+                <TextInput style = {stylesheet._Input_Box_Style}
+                  label="Breed"
+                  autoCapitalize="none"
+                  autoComplete="email"
+                  textContentType="emailAddress"
+                  value={breed}
+                  onFocus={()=>setShift(false)}
+                  onChangeText={(text) => {
+                    setBreed(text);
+                }}
+                />
+           </View>
+          <View style={stylesheet._Text_Field_Gender}>
+            <TextInput
+                style={stylesheet._Input_Box_Style}
+                label="Gender"
+                autoCapitalize="none"
+                autoComplete="email"
+                textContentType="emailAddress"
+                value={gender}
+                onFocus={()=>setShift(false)}
+                onChangeText={(text) => {
+                  setGender(text);
+                }}
+              />
+          </View>
+          <View style={stylesheet._Text_Field_Birthday}>
+            <TextInput
+                style={stylesheet._Input_Box_Style}
+                label="Birthday"
+                autoCapitalize="none"
+                autoComplete="email"
+                textContentType="emailAddress"
+                value={birthday}
+                onFocus={()=>setShift(false)}
+                onChangeText={(text) => {
+                  setBirthday(text);
+                }}
+              />
+          </View>
+          <View style={stylesheet._Text_Field_Location}>
+              <TextInput
+                style={stylesheet._Input_Box_Style}
+                label="Location"
+                autoCapitalize="none"
+                autoComplete="email"
+                textContentType="emailAddress"
+                value={location}
+                onFocus={()=>setShift(true)}
+                onChangeText={(text) => {
+                  setLocation(text);
+                }}
+              />
+           </View>
+           <View style = {stylesheet._Rectangle_39} >
+              <Button 
+                style={[styles.button]} 
+                labelStyle={styles.buttonLabel}
+                onPress={()=>uploadInfo()}> 
+                Continue
+              </Button>
+            </View>
+         
+         
+    </KeyboardAvoidingView>
+      
+ 
    
       // <KeyboardAvoidingView  
       //   behavior={Platform.OS === "ios" ? "padding" : "height"}
       //   enabled keyboardVerticalOffset={Platform.select({ios: 80, android: 500})}
       //   style={stylesheet.test}>
-        <View style={stylesheet._iPhone_11_Pro___X___2} >
-          <Text style={stylesheet.titleText}>Let's get some Info!</Text>
-          <TextInput
-              style={stylesheet._Text_Field_Breed}
-              label="Breed"
-              autoCapitalize="none"
-              autoComplete="email"
-              textContentType="emailAddress"
-              enablesReturnKeyAutomatically
-              value={breed}
-              onChangeText={(text) => {
-                setBreed(text);
-              }}
-            />
-            <TextInput
-              style={stylesheet._Text_Field_Gender}
-              label="Gender"
-              autoCapitalize="none"
-              autoComplete="email"
-              textContentType="emailAddress"
-              enablesReturnKeyAutomatically
-              value={gender}
-              onChangeText={(text) => {
-                setGender(text);
-              }}
-            />
-            <TextInput
-              style={stylesheet._Text_Field_Birthday}
-              label="Birthday"
-              autoCapitalize="none"
-              autoComplete="email"
-              textContentType="emailAddress"
-              enablesReturnKeyAutomatically
-              value={birthday}
-              onChangeText={(text) => {
-                setBirthday(text);
-              }}
-            />
-            <KeyboardAvoidingView 
-            behavior={Platform.OS === "ios" ? "padding" : "height"}
-            style={stylesheet._Text_Field_Location}
-             >
-              <TextInput
-                style={stylesheet._Text_Field_Style}
-                label="Location"
-                autoCapitalize="none"
-                autoComplete="email"
-                textContentType="emailAddress"
-                enablesReturnKeyAutomatically
-                value={location}
-                onChangeText={(text) => {
-                  setLocation(text);
-                }}
-              />
-            </KeyboardAvoidingView>
+    // <SafeAreaView style = {{flex: 1}}>
+    // <KeyboardAvoidingView
+    // behavior={Platform.OS === "ios" ? "padding" : "height"}>
+    //     <View style={stylesheet._iPhone_11_Pro___X___2} >
+    //      
+    //       {/* <KeyboardAvoidingView 
+    //        behavior={Platform.OS === "ios" ? "padding" : "height"}
+    //       style={stylesheet.kav}> */}
+    //       <TextInput
+    //           style={stylesheet._Text_Field_Breed}
+    //           label="Breed"
+    //           autoCapitalize="none"
+    //           autoComplete="email"
+    //           textContentType="emailAddress"
+    //           value={breed}
+    //           onChangeText={(text) => {
+    //             setBreed(text);
+    //           }}
+    //         />
+    //         <TextInput
+    //           style={stylesheet._Text_Field_Gender}
+    //           label="Gender"
+    //           autoCapitalize="none"
+    //           autoComplete="email"
+    //           textContentType="emailAddress"
+    //           value={gender}
+    //           onChangeText={(text) => {
+    //             setGender(text);
+    //           }}
+    //         />
+    //         <TextInput
+    //           style={stylesheet._Text_Field_Birthday}
+    //           label="Birthday"
+    //           autoCapitalize="none"
+    //           autoComplete="email"
+    //           textContentType="emailAddress"
+    //           value={birthday}
+    //           onChangeText={(text) => {
+    //             setBirthday(text);
+    //           }}
+    //         />
+    //         {/* <View
+    //         style={stylesheet._Text_Field_Location}
+    //          > */}
+    //           <TextInput
+    //             style={stylesheet._Text_Field_Style}
+    //             label="Location"
+    //             autoCapitalize="none"
+    //             autoComplete="email"
+    //             textContentType="emailAddress"
+    //             value={location}
+    //             onChangeText={(text) => {
+    //               setLocation(text);
+    //             }}
+    //           />
+    //         {/* </View> */}
+    //         {/* </KeyboardAvoidingView> */}
            
-            <View style = {stylesheet._Rectangle_39} >
-              <Button 
-                style={[styles.button]} 
-                labelStyle={styles.buttonLabel}> 
-                Continue
-              </Button>
-            </View>
-          </View>
-      // </KeyboardAvoidingView>
+    //         <View style = {stylesheet._Rectangle_39} >
+    //           <Button 
+    //             style={[styles.button]} 
+    //             labelStyle={styles.buttonLabel}> 
+    //             Continue
+    //           </Button>
+    //         </View>
+            
+    //     </View>
+                
+    // </KeyboardAvoidingView>
+    //       // </SafeAreaView>
+    
 
     
   );
 };
 
 //todo
-//its working but not expandning in the direction we want
+//change everything to relative
 
 
 
 const stylesheet = StyleSheet.create({
-    test: {
-      height: phoneHeight,
-      width: phoneWidth,
-
-    },
-    titleText:{
-      position: 'absolute',
-      fontFamily: "braveold",
-      fontSize: 50,
-      left: phoneWidth*0.115,
-    },
-    buttonContainer:{
-      borderWidth: 10
-    },
-    _iPhone_11_Pro___X___2: {
-        position: "absolute",
-        width: Dimensions.get("window").width,
-        height: 812,
-        borderRadius: 0,
-        overflow: "hidden",
-        transform: [{ translateX: 0 }, { translateY: 0 }, { rotate: "0deg" }],
-        backgroundColor: "rgba(245, 239, 224, 1)",
-      },
-      _Text_Field_Breed: {
-        position: "absolute",
-        width: boxWidth,
-        height: boxHeight,
-        fontFamily: "Montserrat",
-        borderRadius: 5,
-        borderWidth: 0.5,
-        borderStyle: "solid",
-        borderColor: "rgba(90, 67, 62, 1)",
-        left: phoneWidth*0.115,
-        right: "auto",
-        top: phoneHeight* 0.246,
-        bottom: "auto",
-        transform: [{ translateX: 0 }, { translateY: 0 }, { rotate: "0deg" }],
-        backgroundColor: "rgba(255, 255, 255, 1)",
-      },
-      _Text_Field_Gender : {
-        position: "absolute",
-        width: boxWidth,
-        height: boxHeight,
-        fontFamily: "Montserrat",
-        borderRadius: 5,
-        borderWidth: 0.5,
-        borderStyle: "solid",
-        borderColor: "rgba(90, 67, 62, 1)",
-        left: phoneWidth*0.115,
-        right: "auto",
-        top: phoneHeight*0.331,
-        bottom: "auto",
-        transform: [{ translateX: 0 }, { translateY: 0 }, { rotate: "0deg" }],
-        backgroundColor: "rgba(255, 255, 255, 1)",
-      },
-      _Text_Field_Birthday:{
-        position: "absolute",
-        width: boxWidth,
-        height: boxHeight,
-        fontFamily: "Montserrat",
-        borderRadius: 5,
-        borderWidth: 0.5,
-        borderStyle: "solid",
-        borderColor: "rgba(90, 67, 62, 1)",
-        left: phoneWidth*0.115,
-        right: "auto",
-        top: phoneHeight*0.417,
-        bottom: "auto",
-        transform: [{ translateX: 0 }, { translateY: 0 }, { rotate: "0deg" }],
-        backgroundColor: "rgba(255, 255, 255, 1)",
-      },
-      _Text_Field_Location:{
-        position: "absolute",
-        left: phoneWidth*0.115,
-        right: "auto",
-        top: phoneHeight*0.602,
-        bottom: "auto",
-        borderWidth: 5,
-      },
-      _Text_Field_Style:{
-        width: boxWidth,
-        height: boxHeight,
-        fontFamily: "Montserrat",
-        borderRadius: 5,
-        borderWidth: 0.5,
-        borderStyle: "solid",
-        borderColor: "rgba(90, 67, 62, 1)",
-        transform: [{ translateX: 0 }, { translateY: 0 }, { rotate: "0deg" }],
-        backgroundColor: "rgba(255, 255, 255, 1)",
-      },
-      _Rectangle_39: {
-        position: "absolute",
-        width: 299,
-        height: 56, 
-        right: "auto",
-        top: phoneHeight* 0.756,
-        alignContent: 'center',
-        justifyContent: 'center',
-        alignSelf: 'center',
-      },
+  backGround:{
+    flex: 1,
+    backgroundColor: "rgba(245, 239, 224, 1)",
+  },
+  textView:{
+    marginTop: phoneHeight*0.07,
+  },
+  titleText:{
+    fontFamily: "braveold",
+    fontSize: 50,
+    left: phoneWidth*0.115,
+  },
+ //_Text_Field_##### position the input box relative to the screen 
+  _Text_Field_Breed: {
+    position: "absolute",
+    alignSelf: 'center',
+    top: phoneHeight* 0.306,
+  },
+  _Text_Field_Gender : {
+    position: "absolute",
+    alignSelf: 'center',
+    top: phoneHeight*0.406,
+  },
+  _Text_Field_Birthday:{
+    position: "absolute",
+    alignSelf: 'center',
+    top: phoneHeight*0.506,        
+  },
+  _Text_Field_Location:{
+    position: 'absolute',
+    alignSelf: 'center', 
+    top: phoneHeight*0.606,
+  },
+  //Inputbox style
+  _Input_Box_Style:{
+    fontFamily: "Montserrat",
+    borderRadius: 5,
+    borderWidth: 0.5,
+    borderStyle: "solid",
+    borderColor: "rgba(90, 67, 62, 1)",
+    width: boxWidth,
+    height: boxHeight,
+    transform: [{ translateX: 0 }, { translateY: 0 }, { rotate: "0deg" }],
+    backgroundColor: "rgba(255, 255, 255, 1)",
+  },
+  //button style
+  _Rectangle_39: {
+    position: "absolute",
+    width: 299,
+    height: 56, 
+    right: "auto",
+    top: phoneHeight* 0.756,
+    alignContent: 'center',
+    justifyContent: 'center',
+    alignSelf: 'center',
+  },
+  buttonContainer:{
+    borderWidth: 10
+  },
 })
 
 export default GetInfoScreen;
