2fe8bbf7c0817a2d3e32664fcea9a29d32d7d856
James Dingle <jdingle7@g.ucla.edu>
2022-04-04 16:24:28 -0700

    prettier

--- frontend/screens/Summary.tsx
+++ frontend/screens/Summary.tsx
@@ -1,63 +1,62 @@
-import React from 'react'
-import { StyleSheet, Text, Image, SafeAreaView, FlatList } from 'react-native';
-import { View } from '../components/Themed';
+import React from "react";
+import { StyleSheet, Text, Image, SafeAreaView, FlatList } from "react-native";
+import { View } from "../components/Themed";
 import { Button } from "react-native-paper";
-import styles from '../stylesheets/globalStyles'
+import styles from "../stylesheets/globalStyles";
 
 export default function SummaryPage() {
-  const recipes = [ //test data; get data later
+  const recipes = [
+    //test data; get data later
     {
-        id: 0,
-        title: 'Distance',
-        data: '0.20 mi',
+      id: 0,
+      title: "Distance",
+      data: "0.20 mi",
     },
     {
-        id: 1,
-        title: 'Time',
-        data: '00:04:32',
+      id: 1,
+      title: "Time",
+      data: "00:04:32",
     },
     {
-        id: 2,
-        title: 'Avg Speed (mph)',
-        data: '2.65',
+      id: 2,
+      title: "Avg Speed (mph)",
+      data: "2.65",
     },
     {
-        id: 3,
-        title: 'Pee Stops',
-        data: '5',
+      id: 3,
+      title: "Pee Stops",
+      data: "5",
     },
     {
-        id: 4,
-        title: 'Poop Drops',
-        data: '3',
+      id: 4,
+      title: "Poop Drops",
+      data: "3",
     },
     {
-        id: 5,
-        title: 'Water Breaks',
-        data: '2',
-    }
+      id: 5,
+      title: "Water Breaks",
+      data: "2",
+    },
   ];
 
   return (
-    <SafeAreaView style={styles.container} >
+    <SafeAreaView style={styles.container}>
       <View style={localStyles.topContainer}>
         <View style={styles.titleView}>
-          <Text style={styles.title} >
-            Summary
-          </Text>
+          <Text style={styles.title}>Summary</Text>
         </View>
-        <View style={{height: '65%'}}>
-          <Image 
+        <View style={{ height: "65%" }}>
+          <Image
             style={styles.image}
-            source={require('../assets/images/summary-image.png')} 
+            source={require("../assets/images/summary-image.png")}
             resizeMode="contain"
           />
         </View>
         <View style={localStyles.speedBarContainer}>
           <Text style={styles.p}>Slow</Text>
-          <Image 
+          <Image
             style={styles.image}
-            source={require('../assets/images/speed-bar.png')} 
+            source={require("../assets/images/speed-bar.png")}
             resizeMode="contain"
           />
           <Text style={styles.p}>Fast</Text>
@@ -72,25 +71,25 @@ export default function SummaryPage() {
           style={styles.list}
           data={recipes}
           renderItem={(obj) => {
-              return (
-                  <View style={styles.row}>
-                    <View style={styles.cell}>
-                      <Text style={styles.p}>{obj.item.title}</Text>
-                    </View>
-                    <View style={styles.cell}>
-                      <Text style={[styles.p,  {textAlign: 'right'}]}>{obj.item.data}</Text>
-                    </View>
-                  </View>
-              );
+            return (
+              <View style={styles.row}>
+                <View style={styles.cell}>
+                  <Text style={styles.p}>{obj.item.title}</Text>
+                </View>
+                <View style={styles.cell}>
+                  <Text style={[styles.p, { textAlign: "right" }]}>
+                    {obj.item.data}
+                  </Text>
+                </View>
+              </View>
+            );
           }}
-          />
+        />
 
-      <Button style={styles.button} labelStyle={styles.buttonLabel}> 
-        Continue
-      </Button>
+        <Button style={styles.button} labelStyle={styles.buttonLabel}>
+          Continue
+        </Button>
       </View>
-      
-      
     </SafeAreaView>
   );
 }
@@ -98,7 +97,7 @@ export default function SummaryPage() {
 const localStyles = StyleSheet.create({
   topContainer: {
     flex: 1,
-    flexDirection: 'column',
+    flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "rgb(244,238,225)",
@@ -107,17 +106,17 @@ const localStyles = StyleSheet.create({
   },
   bottomContainer: {
     flex: 1,
-    width: '100%',
-    backgroundColor: 'rgb(252,251,247)',
+    width: "100%",
+    backgroundColor: "rgb(252,251,247)",
     // borderWidth: 3,
     // borderColor: "pink",
   },
   speedBarContainer: {
-    flex: 1, 
-    flexDirection: 'row', 
-    width: '80%', 
-    justifyContent: 'center', 
-    alignItems: 'center', 
+    flex: 1,
+    flexDirection: "row",
+    width: "80%",
+    justifyContent: "center",
+    alignItems: "center",
     // borderWidth: 1
   },
 });
