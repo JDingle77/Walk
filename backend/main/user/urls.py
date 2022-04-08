53dc7d8dda68b6690683ca1d5d539026c3d4e1f8
jainmehul0518 <jainmehul0518@gmail.com>
2022-04-07 23:52:04 -0700

    Implemented DogProfile functionality for create account request. Added GET dogProfile request handler

--- backend/main/user/urls.py
+++ backend/main/user/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.user, name="all_users"),
     path('<uuid:user_id>/', views.handle_user, name="get_user"),
+    path('dogProfile/', views.dog, name ="get_dog_profile")
 ]
