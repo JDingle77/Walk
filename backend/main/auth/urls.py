3646803d50ada67ae6300956bdd68c86021dd0ca
jainmehul0518 <jainmehul0518@gmail.com>
2022-04-18 18:56:34 -0700

    Implemented email validation functionality

--- backend/main/auth/urls.py
+++ backend/main/auth/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import SimpleRouter
-from .views import LoginViewSet, RegistrationViewSet, RefreshViewSet
+from .views import LoginViewSet, RegistrationViewSet, RefreshViewSet, ValidEmail
 from django.urls import path
 
 routes = SimpleRouter()  # Use SimpleRouter to create url patterns for class-based views
@@ -8,4 +8,5 @@ routes.register(r'login', LoginViewSet, basename="auth-login"),
 routes.register(r'register', RegistrationViewSet, basename="auth-register")
 routes.register(r'refresh', RefreshViewSet, basename="auth-refresh")
 
-urlpatterns = [*routes.urls]
+urlpatterns = [*routes.urls, 
+    path('validEmail/', ValidEmail, name="validate_email"),]
