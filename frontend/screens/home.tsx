ed19026cf454a2b69e83a09e7b95c49f1880e506
jainmehul0518 <jainmehul0518@gmail.com>
2022-06-24 16:23:52 -0700

    Reverted home screen to original implementation

--- frontend/screens/home.tsx
+++ frontend/screens/home.tsx
@@ -1,59 +1,14 @@
 import * as React from "react";
 import { SafeAreaView, StyleSheet } from "react-native";
 import { Text, View } from "../components/Themed";
-import { Button } from "react-native-paper";
-import {getValueFor} from "../functions/SecureStore";
-import { refreshAccess } from "../functions/RefreshHandler";
-import { RootStackParamList } from "../types";
-import { StackNavigationProp } from "@react-navigation/stack";
 
-type NavigationProp = StackNavigationProp<RootStackParamList, "Home">;
-
-type Props = {
-  navigation: NavigationProp;
-};
-
-const Home = ({ navigation }: Props) => {
+const Home = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>This is the Home Page</Text>
-      <Button
-        onPress={() => getValueFor("access_token").then((response) => console.log(response))
-        }>
-          Hello World
-      </Button>
-      <Button onPress={async () => {
-          refreshAccess().then(() =>
-          {
-            getSummaryStats();
-          }).catch((err) =>
-          {
-            navigation.navigate("Login");
-          })
-      }}
-      >
-          Test Refresh
-      </Button>
     </View>
   );
 };
-
-async function getSummaryStats() {
-  let access_token = await getValueFor("access_token");
-  fetch("http://localhost:8000/maps/get_summary_statistics/", {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + access_token,
-    },
-  })
-    .then((response) => response.json())
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((err) => console.error(err));
-}
 
 const styles = StyleSheet.create({
   container: {
